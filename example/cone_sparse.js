37f5ec53d6a7e0a9ae1ce4185191d5ef304e20e7
Ilmari Heikkinen <ilmari@fhtr.net>
2018-05-20 05:52:51 +0200

    less es6

--- example/cone_sparse.js
+++ example/cone_sparse.js
@@ -69,7 +69,7 @@ var mesh = createMesh(gl, conePlot)
 
 var select = createSelect(gl, [canvas.width, canvas.height])
 var tickSpacing = 5;
-var ticks = bounds[0].map((v,i) => {
+var ticks = bounds[0].map(function(v,i) {
   var arr = [];
   var firstTick = Math.ceil(bounds[0][i] / tickSpacing) * tickSpacing;
   var lastTick = Math.floor(bounds[1][i] / tickSpacing) * tickSpacing;
