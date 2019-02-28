003a0bb3b20940262293aa6693ce74378782fad8
archmoj <mojtaba@plot.ly>
2019-02-28 15:14:58 -0500

    try almost identical fragment shader with gl-mesh3d

--- lib/triangle-fragment.glsl
+++ lib/triangle-fragment.glsl
@@ -1,3 +1,5 @@
+#extension GL_OES_standard_derivatives : enable
+
 precision mediump float;
 
 #pragma glslify: cookTorrance = require(glsl-specular-cook-torrance)
@@ -21,21 +23,19 @@ varying vec4 f_color;
 varying vec2 f_uv;
 
 void main() {
-
-  if (outOfRange(clipBounds[0], clipBounds[1], f_position)) discard;
-
+  //if (outOfRange(clipBounds[0], clipBounds[1], f_position)) discard;
   vec3 N = normalize(f_normal);
   vec3 L = normalize(f_lightDirection);
   vec3 V = normalize(f_eyeDirection);
 
-  if(!gl_FrontFacing) {
+  if(gl_FrontFacing) {
     N = -N;
   }
 
-  float specular = cookTorrance(L, V, N, roughness, fresnel);
+  float specular = min(1.0, max(0.0, cookTorrance(L, V, N, roughness, fresnel)));
   float diffuse  = min(kambient + kdiffuse * max(dot(N, L), 0.0), 1.0);
 
-  vec4 surfaceColor =  texture2D(texture, f_uv);
+  vec4 surfaceColor = f_color * texture2D(texture, f_uv);
   vec4 litColor = surfaceColor.a * vec4(diffuse * surfaceColor.rgb + kspecular * vec3(1,1,1) * specular,  1.0);
 
   gl_FragColor = litColor * opacity;
